d824a7049f8dedcd12bd6addf58fdac5cb809dd0
Vinicius Camargo <111100532+vinicamargors@users.noreply.github.com>
2022-11-18 14:08:56 -0300

    Update dbConfig.js

--- src/config/dbConfig.js
+++ src/config/dbConfig.js
@@ -1,6 +1,6 @@
 const mongoose = require('mongoose'); //instancia e requere o mongoose 
 
-const dbConfig = 'mongodb+srv://camargovini:re250295@cluster0.pt2ey4a.mongodb.net/annotations?retryWrites=true&w=majority';//link de conexão que o proprio mongodb fornece
+const dbConfig = 'mongodb+srv://seuusuário:suasenha@cluster0.pt2ey4a.mongodb.net/annotations?retryWrites=true&w=majority';//link de conexão que o proprio mongodb fornece
 
 const connection = mongoose.connect(dbConfig, {
     useNewUrlParser: true, //padrão do mongoose colocar esses
